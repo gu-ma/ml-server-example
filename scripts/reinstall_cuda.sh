183fd75f0a6de7ed936253735ff208620315b1ea
Guillaume Massol <guillaume.massol@gmail.com>
2020-11-17 17:36:20 +0100

    Add notes to scripts

--- scripts/reinstall_cuda.sh
+++ scripts/reinstall_cuda.sh
@@ -1,11 +1,10 @@
-# Install CUDA 10.1 (Ubuntu 18.04)
-apt-get --purge remove "*cublas*" "*cufft*" "*curand*" "*cusolver*" "*cusparse*" "*npp*" "*nvjpeg*" "cuda*" "nsight*" && \
-apt-get autoremove && \
+# Install CUDA 10.1 (Ubuntu 18.04) - DOESNT WORK ON THE PAPERSPACE MACHINES
+sudo apt-get --purge remove "*cublas*" "cuda*" "nvidia-cuda*" && \
+sudo apt-get autoremove && \
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.105-1_amd64.deb && \
-dpkg -i cuda-repo-ubuntu1804_10.1.105-1_amd64.deb && \
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub && \
-apt-get update && \
-apt-get install cuda-10-1 && \
+sudo dpkg -i cuda-repo-ubuntu1804_10.1.105-1_amd64.deb && \
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub && \
+sudo apt-get install cuda-10-1 && \
 rm cuda-repo-ubuntu1804_10.1.105-1_amd64.deb && \
 echo "export PATH=\"/usr/local/cuda-10.1/bin:\$PATH\" \nexport LD_LIBRARY_PATH=\"/usr/local/cuda-10.1/lib64:\$LD_LIBRARY_PATH\"" >> ~/.bashrc && \
-source .bashrc && \
+source ~/.bashrc
--- scripts/reinstall_nvidia_drivers.sh
+++ scripts/reinstall_nvidia_drivers.sh
@@ -1,5 +1,6 @@
-# Install driver 455 version (Ubuntu 18.04)
-apt-get --purge remove "*nvidia*" && \
-apt-get autoremove && \
-add-apt-repository ppa:graphics-drivers/ppa &&\
-apt install nvidia-455
+# Install driver 455 version (Ubuntu 18.04) - DOESNT WORK ON THE PAPERSPACE MACHINES
+sudo apt-get --purge remove "*nvidia*" && \
+sudo apt-get autoremove && \
+sudo add-apt-repository ppa:graphics-drivers/ppa && \
+sudo apt-get update && \
+sudo apt install nvidia-455
